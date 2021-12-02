2822bb3b8deaa2fcda847345b40d902b8fe0ccac
Bryant Mercedes <bryantmercedescanela@gmail.com>
2021-12-02 14:36:28 -0500

    number aren

--- Adding.js
+++ Adding.js
@@ -1,9 +1,9 @@
 const message = window.prompt("give me a number");
-        const number = Number(message);
-        let message2 = "";
-        if(isNaN(message)){
-           message2 = "Please enter #";
-        } else{
-           message2 = `number is ${message *2}`;
-        }
-        window.alert(message2)
+const number = Number(message);
+let message2 = "";
+if (isNaN(message)) {
+  message2 = "Please enter #";
+} else {
+  message2 = `number are ${message * 2}`;
+}
+window.alert(message2);
